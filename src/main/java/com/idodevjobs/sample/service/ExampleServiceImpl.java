91f462ee707405afc8474e8530d9f21aaf119c77
taojigu <taojigu@163.com>
2016-01-31 22:00:38 +0800

    add project

---
+++ src/main/java/com/idodevjobs/sample/service/ExampleServiceImpl.java
@@ -0,0 +1,14 @@
+package com.idodevjobs.sample.service;
+
+import com.idodevjobs.sample.mode.ExampleModel;
+import org.springframework.stereotype.Service;
+ 
+@Service("exampleService")
+public class ExampleServiceImpl implements ExampleService {
+ 
+    //@Override
+    public ExampleModel get(String modelId) {
+    	
+        return new ExampleModel("example", 1001);
+    }
+}
